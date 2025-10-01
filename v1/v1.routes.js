531fb72ac177256e926c037925aae55920c63447
AlanLangelan <alanlangelan.2016@gmail.com>
2025-10-01 09:09:13 -0300

    Fix astah

--- v1/v1.routes.js
+++ v1/v1.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
+import { authMiddleware } from './middlewares/auth.middleware.js';
 const router = express.Router();
-
 router.use("/auth", authRoutes);
+router.use(authMiddleware);
 
 export default router;
